a5d0f9618bf0a5fde89d7fd637e707a6116a326e
Melih Birim <398797+melihbirim@users.noreply.github.com>
2026-07-17 15:42:03 +0100

    chore: update formula to v1.7.0

--- Formula/csvql.rb
+++ Formula/csvql.rb
@@ -1,24 +1,24 @@
 class Csvql < Formula
   desc "Ultra-fast SQL query engine for CSV files with SIMD parsing and parallel execution"
   homepage "https://github.com/melihbirim/csvql"
-  version "1.6.1"
+  version "1.7.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/melihbirim/csvql/releases/download/v1.6.1/csvql-macos-aarch64.tar.gz"
-      sha256 "9e3e9e14e3733029f27fca082fa80478ba520c535c9bfdb1b712a6589e235299"
+      url "https://github.com/melihbirim/csvql/releases/download/v1.7.0/csvql-macos-aarch64.tar.gz"
+      sha256 "828b0de387318a0174781f1f64fe926f4679693881252aaf4f64adfbf37d413d"
     end
     on_intel do
-      url "https://github.com/melihbirim/csvql/releases/download/v1.6.1/csvql-macos-x86_64.tar.gz"
-      sha256 "90f6bae4bfdf7303efd5c7893332fe89d41e751ccf504b6c52fcb25419f84b49"
+      url "https://github.com/melihbirim/csvql/releases/download/v1.7.0/csvql-macos-x86_64.tar.gz"
+      sha256 "2d989c8ee69871afd894d981c4ba88a2dbc1879472912c90ec1f093515871c10"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/melihbirim/csvql/releases/download/v1.6.1/csvql-linux-x86_64.tar.gz"
-      sha256 "f238998c9eaf533788cb1a2cbe07cc97d3a13365cb5c92e947f74ff68e46667c"
+      url "https://github.com/melihbirim/csvql/releases/download/v1.7.0/csvql-linux-x86_64.tar.gz"
+      sha256 "71cd9af47e825b84260b4795a045dd9780f6a38dda1cd13beb1931f9118ccf7c"
     end
   end
 
