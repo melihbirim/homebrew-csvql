00b902947c400d074d92b8258aa9e5ef00900764
Melih Birim <398797+melihbirim@users.noreply.github.com>
2026-09-01 14:49:49 +0300

    chore: update formula to v2.6.2

--- Formula/csvql.rb
+++ Formula/csvql.rb
@@ -7,18 +7,18 @@ class Csvql < Formula
   on_macos do
     on_arm do
       url "https://github.com/melihbirim/csvql/releases/download/v2.6.2/csvql-macos-aarch64.tar.gz"
-      sha256 "4e4fa4f0fa9a162c9c69932daa4f30a001a9faad49aba846534270c9f9d10ccf"
+      sha256 "f01a2f99b4ed7bfc999d63f683360115b6536a0f617878236bc9d30d40a15eeb"
     end
     on_intel do
       url "https://github.com/melihbirim/csvql/releases/download/v2.6.2/csvql-macos-x86_64.tar.gz"
-      sha256 "f0c54a931a6c5e195276321b1ba0eaa4e078231525c674ac2147443c8b906346"
+      sha256 "217e890e16a3c51a3935f3bd21dfca02e3b41cdf3753c137205600a0d0e21bbb"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/melihbirim/csvql/releases/download/v2.6.2/csvql-linux-x86_64.tar.gz"
-      sha256 "5c248b82e65fc621ccb3abf413eb62b53f55109fb0da4e7a6aaacf236789e043"
+      sha256 "6ff06cca42f0eb82483fc79848502eb7786b2c66775901c9ac5dea96b5154e0a"
     end
   end
 
